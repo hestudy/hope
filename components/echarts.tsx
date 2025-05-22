f3b47a959f1ad011b1918a2ff4c019b745191d9b
hestudy <1467602180@qq.com>
2025-05-22 10:36:19 +0800

    ✨ feat(echarts): 新增ECharts图表组件并添加echarts依赖
    新增了一个可复用的ECharts图表React组件，支持响应式调整大小和自动清理。同时在package.json中添加了echarts 5.6.0作为项目依赖。

---
+++ components/echarts.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import * as echarts from "echarts";
+import React, { useEffect, useRef } from "react";
+
+export default function Echarts(props: {
+  option: echarts.EChartsOption;
+  style?: React.CSSProperties;
+  className?: string;
+}) {
+  const chartRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const chart = echarts.init(chartRef.current);
+      chart.setOption(props.option);
+      window.addEventListener("resize", () => {
+        chart.resize();
+      });
+      return () => {
+        window.removeEventListener("resize", () => {
+          chart.resize();
+        });
+        chart.dispose();
+      };
+    }
+  }, []);
+
+  return <div ref={chartRef} style={props.style} className={props.className} />;
+}
